9d6d21e49c23d48306f37c66633fa5f95ea1e96b
ItsAJ1005 <ajharshvardhan123@gmail.com>
2024-07-15 17:12:19 +0530

    Update - file structure

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Get Your Prompts",
+    description: "Discover and Share AI prompts."
+}  
+
+const layout = () => {
+  return (
+    <html len='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+
+                
+            </div>
+        </body>
+    </html>
+  )
+}
+
+export default layout
