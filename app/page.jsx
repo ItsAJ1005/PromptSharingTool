fccd21aa322fe47328a1b458bb5d27d704ea5966
ItsAJ1005 <ajharshvardhan123@gmail.com>
2024-07-17 12:01:12 +0530

    Add - hero section head

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,16 @@
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover, Reuse & Share
+        <br className="max-md:hidden"/>
+        <span className="green_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Get Your Prompts is an open-source AI prompting tool for modern world to discover, create and share useful prompts</p>
+
+      {/* Feed */}
+    </section>
   )
 }
 
